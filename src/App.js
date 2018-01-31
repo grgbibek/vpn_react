9e447d50d9627a82b4e28629edb84f60512d7c9b
AmirStha <amirshresthapj@gmail.com>
2018-01-31 17:26:15 +0545

    login working through router

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import {BrowserRouter as Router, Link, Route} from 'react-router-dom';
 
 
 import './App.css'
-import About from './components/About';
-import Home from './components/Home';
-import Topics from './components/Topics';
+
+import Login from './Login/login';
+
 
 
 class App extends Component {
@@ -13,14 +13,14 @@ class App extends Component {
     return (
       <Router>
         <div className="App">
-            <ul>
+            {/*<ul>
               <li><Link to='/'>Home</Link></li>
               <li><Link to='/about'>About</Link></li>
               <li><Link to='/topics'>Topics</Link></li>
-            </ul>
-            <Route  path='/' exact component={Home} />
-            <Route  path='/about' component={About} />
-            <Route  path='/topics' component={Topics} />
+            </ul>*/}
+            <Route  path='/' exact component={Login} />
+            {/*<Route  path='/about' component={About} />
+            <Route  path='/topics' component={Topics} />*/}
         </div>
       </Router>
     );
